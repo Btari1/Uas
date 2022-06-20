7cdf5f331c889c31d75d23855bb92a58356db1bb
Btari1 <94272896+Btari1@users.noreply.github.com>
2022-06-20 12:33:20 +0700

    Add files via upload

---
+++ UasWeb2/uascrud/index.php
@@ -0,0 +1,17 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?= template_header('Home') ?>
+
+<div class="content">
+    <h2>Home</h2>
+    <p>Ahmad Zafar</p>
+    <p>20200810047</p>
+    <p>TI C 2020</p>
+</div>
+
+<?= template_footer() ?>
